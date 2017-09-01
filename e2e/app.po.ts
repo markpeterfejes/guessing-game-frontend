fa8a8ba5d0cd9d1d1efe4d18789dcf632b128c97
MARKPETERFEJES\fejes <mfejes@korzo.co>
2017-09-01 11:49:24 +0200

    Small e2e testing

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,49 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element } from "protractor";
 
-export class AppPage {
-  navigateTo() {
-    return browser.get('/');
-  }
+export class AppPage
+{
+    navigateTo()
+    {
+        return browser.get("/");
+    }
 
-  getParagraphText() {
-    return element(by.css('second-test-root h1')).getText();
-  }
+    getWelcomeText()
+    {
+        return element(by.css(".container-fluid h1")).getText();
+    }
+
+    getInstructionText()
+    {
+        return element(by.css(".container-fluid h4")).getText();
+    }
+
+    getGuessInput()
+    {
+        return element(by.css("input#guessInput"));
+    }
+
+    getFirstGuessListItem()
+    {
+        return element(by.css("ul#guessList li.guess-list-item")).getText();
+    }
+
+    getAllGuessListItems()
+    {
+        return element.all(by.css("ul#guessList li.guess-list-item"));
+    }
+
+    getGuessButton()
+    {
+        return element(by.css("button#guessButton"));
+    }
+
+    getDisabledGuessButton()
+    {
+        return element.all(by.css("button#guessButton.disabled"));
+    }
+
+    getFailureEndMessageDiv()
+    {
+        return element.all(by.css("div.end-message.alert-danger"));
+    }
 }

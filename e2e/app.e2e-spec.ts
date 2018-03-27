ff7b849cd136e9910f063ae1a84233640faab7b1
Mark Peter Fejes <mark.fejes@simpledcard.com>
2018-03-27 10:16:10 +0200

    Removed shitty e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -14,34 +14,4 @@ describe("second-test App", () =>
         page.navigateTo();
         expect(page.getWelcomeText()).toEqual("Welcome to the guessing game!");
     });
-
-    it("should display the instructions", () =>
-    {
-        page.navigateTo();
-        expect(page.getInstructionText()).toEqual("Please Enter your guess between 0 and 100");
-    });
-
-    it("should guess a number and diplay it in the list", () =>
-    {
-        page.navigateTo();
-        page.getGuessInput().sendKeys(42);
-        page.getGuessButton().click();
-
-        expect(page.getFirstGuessListItem()).toEqual("42");
-    });
-
-    it("should loose a game and have all the guesses in the list", () =>
-    {
-        page.navigateTo();
-        page.getGuessInput().sendKeys(122);
-        page.getGuessButton().click();
-        page.getGuessInput().sendKeys(122);
-        page.getGuessButton().click();
-        page.getGuessInput().sendKeys(122);
-        page.getGuessButton().click();
-
-        expect(page.getAllGuessListItems().count()).toEqual(3);
-        expect(page.getDisabledGuessButton().count()).toEqual(1);
-        expect(page.getFailureEndMessageDiv().count()).toEqual(1);
-    });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -11,39 +11,4 @@ export class AppPage
     {
         return element(by.css(".container-fluid h1")).getText();
     }
-
-    getInstructionText()
-    {
-        return element(by.css(".container-fluid h4")).getText();
-    }
-
-    getGuessInput()
-    {
-        return element(by.css("input#guessInput"));
-    }
-
-    getFirstGuessListItem()
-    {
-        return element(by.css("ul#guessList li.guess-list-item")).getText();
-    }
-
-    getAllGuessListItems()
-    {
-        return element.all(by.css("ul#guessList li.guess-list-item"));
-    }
-
-    getGuessButton()
-    {
-        return element(by.css("button#guessButton"));
-    }
-
-    getDisabledGuessButton()
-    {
-        return element.all(by.css("button#guessButton.disabled"));
-    }
-
-    getFailureEndMessageDiv()
-    {
-        return element.all(by.css("div.end-message.alert-danger"));
-    }
 }
